205f46e51c495797d23fecb43867d5ae8fc6c783
Daniel Cuesta <danicuesta00@gmail.com>
2019-01-09 13:23:19 +0100

    Deshabilitar imagen alternativa en pantalla táctil

--- _dev/js/sabanasblancas/replace-image-effect.js
+++ _dev/js/sabanasblancas/replace-image-effect.js
@@ -1,17 +1,34 @@
-(function() {
+function is_touch_device() {
+    var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+    var mq = function(query) {
+      return window.matchMedia(query).matches;
+    }
+  
+    if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+      return true;
+    }
+  
+    // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+    // https://git.io/vznFH
+    var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+    return mq(query);
+}
 
-    $(window).load(function() {
-        $('img[data-alternative-image]').each(function(){
-            $this = $(this);
-            var image1 = $this.attr('src');
-            var image2 = $this.attr('data-alternative-image');
-            $this.hover(function(){
-                $(this).attr('src', image2);
-            }, function() {
-                $(this).attr('src', image1);
-            })
-        });
+$(window).load(function() {
+
+    if (is_touch_device()) {
+        return;
+    }
+
+    $('img[data-alternative-image]').each(function(){
+        $this = $(this);
+        var image1 = $this.attr('src');
+        var image2 = $this.attr('data-alternative-image');
+        $this.hover(function(){
+            $(this).attr('src', image2);
+        }, function() {
+            $(this).attr('src', image1);
+        })
     });
-
-})();
+});
 
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1851,20 +1851,39 @@ jquery__WEBPACK_IMPORTED_MODULE_0___default()(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-/* WEBPACK VAR INJECTION */(function($) {(function () {
-  $(window).load(function () {
-    $('img[data-alternative-image]').each(function () {
-      $this = $(this);
-      var image1 = $this.attr('src');
-      var image2 = $this.attr('data-alternative-image');
-      $this.hover(function () {
-        $(this).attr('src', image2);
-      }, function () {
-        $(this).attr('src', image1);
-      });
+/* WEBPACK VAR INJECTION */(function($) {function is_touch_device() {
+  var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ');
+
+  var mq = function mq(query) {
+    return window.matchMedia(query).matches;
+  };
+
+  if ('ontouchstart' in window || window.DocumentTouch && document instanceof DocumentTouch) {
+    return true;
+  } // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+  // https://git.io/vznFH
+
+
+  var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('');
+  return mq(query);
+}
+
+$(window).load(function () {
+  if (is_touch_device()) {
+    return;
+  }
+
+  $('img[data-alternative-image]').each(function () {
+    $this = $(this);
+    var image1 = $this.attr('src');
+    var image2 = $this.attr('data-alternative-image');
+    $this.hover(function () {
+      $(this).attr('src', image2);
+    }, function () {
+      $(this).attr('src', image1);
     });
   });
-})();
+});
 /* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! jquery */ "jquery")))
 
 /***/ }),
